aa471b91819f9c4d1aa7eb9f1b0327a1a0a34619
w7oleg <68770117+w7oleg@users.noreply.github.com>
2021-09-10 11:02:54 +0300

    Delete src/test/java directory

--- src/test/java/brudingiton/enums/BashColor.java
+++
@@ -1,8 +0,0 @@
-package brudingiton.enums;
-
-public class BashColor {
-    public static final String KW2 = "kw2"; //#c20cb9
-    public static final String RE5 = "re5"; //#660033
-    public static final String ST0 = "st0"; //#ff0000
-    public static final String BR0 = "br0"; //#7a0874
-}
